1b7d9249e6a2985bc38078bbdc9c5e99e5280c2d
skunbydev <Skunbydev@gmail.com>
2024-01-01 02:03:30 -0300

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/pages/login.css';
+
+const Login = () => {
+  return (
+    <div>
+      <div className="login container">
+        <div className="login-photo">
+          <h1>oi</h1>
+        </div>
+        <div className="login-inputs"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
