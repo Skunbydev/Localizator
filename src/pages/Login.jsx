4798d24676da25d85587e5ade427d516e10e93bc
skunbydev <Skunbydev@gmail.com>
2024-01-01 02:18:05 -0300

    Adding life to login css.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../css/pages/login.css';
 
+import loginPhoto from '../assets/login-photo.jpeg';
+
 const Login = () => {
   return (
     <div>
-      <div className="login container">
+      <div className="login-container">
         <div className="login-photo">
-          <h1>oi</h1>
+          <img src={loginPhoto} alt="login-photo" width={650} height={396} />
         </div>
-        <div className="login-inputs"></div>
+        <div className="login-inputs">
+          <h1>Seja bem vindo!</h1>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+          <label htmlFor="password">Senha:</label>
+          <input type="password" />
+        </div>
       </div>
     </div>
   );
